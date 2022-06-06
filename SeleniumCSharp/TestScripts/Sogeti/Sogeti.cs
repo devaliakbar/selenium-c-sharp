6c4a0acc9b0cd6bb8f990f95aec6b48db7a7b814
devaliakbar <dev.aliakbar@gmail.com>
2022-06-06 16:27:33 +0200

    Task2 completed

--- SeleniumCSharp/TestScripts/Sogeti/Sogeti.cs
+++ SeleniumCSharp/TestScripts/Sogeti/Sogeti.cs
@@ -1,7 +1,9 @@
 ﻿
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using SeleniumCSharp.TestScripts.Sogeti;
+using SeleniumCSharp.Utilities;
 
 namespace SeleniumCSharp;
 
@@ -29,8 +31,44 @@ public class Sogeti : SogetiDriver
 
         new Actions(driver).MoveToElement(serviceText).Perform();
 
-        //Verifying Service is selected by checking it's color
+        //Verifying Automation is selected by checking it's color
         var automationText = driver.FindElement(By.XPath("//a[text()='Automation' and @class='subMenuLink']"));
-        Assert.That(serviceText.GetCssValue("color"), Is.EqualTo(selectedTextColor));
+        Assert.That(automationText.GetCssValue("color"), Is.EqualTo(selectedTextColor));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        driver.FindElement(By.ClassName("acceptCookie")).Click();
+
+        var element = driver.FindElement(By.ClassName("level2"));
+        new Actions(driver).MoveToElement(element).Perform();
+
+        driver.FindElement(By.XPath("//a[text()='Automation' and @class='subMenuLink']")).Click();
+
+        driver.FindElement(By.Id("4ff2ed4d-4861-4914-86eb-87dfa65876d8")).SendKeys(TestUtilities.GenerateRandomString());
+
+        driver.FindElement(By.Id("11ce8b49-5298-491a-aebe-d0900d6f49a7")).SendKeys(TestUtilities.GenerateRandomString());
+
+        driver.FindElement(By.Id("056d8435-4d06-44f3-896a-d7b0bf4d37b2")).SendKeys(TestUtilities.GenerateRandomEmail());
+
+        driver.FindElement(By.Id("755aa064-7be2-432b-b8a2-805b5f4f9384")).SendKeys(TestUtilities.GenerateRandomString());
+
+        driver.FindElement(By.Id("703dedb1-a413-4e71-9785-586d609def60")).SendKeys(TestUtilities.GenerateRandomString());
+
+        new SelectElement(driver.FindElement(By.Id("e74d82fb-949d-40e5-8fd2-4a876319c45a"))).SelectByValue("Germany");
+
+        driver.FindElement(By.Id("88459d00-b812-459a-99e4-5dc6eff2aa19")).SendKeys(TestUtilities.GenerateRandomString());
+
+        driver.FindElement(By.XPath("//label[@for='__field_1239350']")).Click();
+
+        // Here captcha need to solve manually within 10 seconds
+        // driver.FindElement(By.XPath(".//*[@class='rc-anchor-content']")).Click();
+        Thread.Sleep(TimeSpan.FromSeconds(10));
+
+        driver.FindElement(By.Id("b35711ee-b569-48b4-8ec4-6476dbf61ef8")).Click();
+
+
+        driver.FindElement(By.XPath("//p[text()='Thank you for contacting us.']"));
     }
 }
---
+++ SeleniumCSharp/Utilities/TestUtilities.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace SeleniumCSharp.Utilities
+{
+    public static class TestUtilities
+    {
+        private static readonly Random _random = new();
+
+        public static string GenerateRandomString(int size = 6)
+        {
+            var builder = new StringBuilder(size);
+
+            char offset = 'a';
+            const int lettersOffset = 26;
+
+            for (var i = 0; i < size; i++)
+            {
+                var character = (char)_random.Next(offset, offset + lettersOffset);
+                builder.Append(character);
+            }
+
+            return builder.ToString().ToLower();
+        }
+
+        public static string GenerateRandomEmail()
+        {
+            int randomInt = _random.Next(1000);
+            return "username" + randomInt + "@gmail.com";
+        }
+    }
+}
