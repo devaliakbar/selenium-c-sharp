949e467194eae3a7919bf7a52244d576a265de36
devaliakbar <dev.aliakbar@gmail.com>
2022-06-06 14:38:37 +0200

    Task1Completed

--- SeleniumCSharp/TestScripts/Sogeti/Sogeti.cs
+++ SeleniumCSharp/TestScripts/Sogeti/Sogeti.cs
@@ -1,15 +1,36 @@
-﻿namespace SeleniumCSharp;
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using SeleniumCSharp.TestScripts.Sogeti;
 
-public class Tests
+namespace SeleniumCSharp;
+
+public class Sogeti : SogetiDriver
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        driver.FindElement(By.ClassName("acceptCookie")).Click();
+
+        var element = driver.FindElement(By.ClassName("level2"));
+        new Actions(driver).MoveToElement(element).Perform();
+
+        driver.FindElement(By.XPath("//a[text()='Automation' and @class='subMenuLink']")).Click();
+
+        //Verifying successfully naviagted to automation page
+        Assert.That(driver.Url, Is.EqualTo("https://www.sogeti.com/services/automation/"));
+
+
+        const string selectedTextColor = "rgba(255, 48, 76, 1)";
+
+        //Verifying Service is selected by checking it's color
+        var serviceText = driver.FindElement(By.XPath("//span[text()='Services']"));
+        Assert.That(serviceText.GetCssValue("color"), Is.EqualTo(selectedTextColor));
+
+        new Actions(driver).MoveToElement(serviceText).Perform();
+
+        //Verifying Service is selected by checking it's color
+        var automationText = driver.FindElement(By.XPath("//a[text()='Automation' and @class='subMenuLink']"));
+        Assert.That(serviceText.GetCssValue("color"), Is.EqualTo(selectedTextColor));
     }
 }
---
+++ SeleniumCSharp/TestScripts/Sogeti/SogetiDriver.cs
@@ -0,0 +1,25 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharp.TestScripts.Sogeti
+{
+    public class SogetiDriver
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.sogeti.com/";
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
+
